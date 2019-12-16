f127b19116bbc30992efe8bbbe0c979331d1f52f
aashutoshjaswani <ashutosh.jaswani444@gmail.com>
2019-12-16 14:50:23 +0530

    drop-down

--- src/components/Studentdata.js
+++ src/components/Studentdata.js
@@ -1,62 +1,101 @@
 import React, { Component } from 'react'
 import './filter.css';
+import './Studentdata.css';
 
 class Studentdata extends Component {
    constructor(props) {
       super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
       this.state = { //state is by default an object
          students: [
-           
+          function myFunction() {
+            
+          }
          ]
       }
    }
+
+
+   handleSubmit4(event) {
+     
+    var x = document.getElementById("slct").value;
+            document.getElementById("demo").innerHTML = x;
+
+event.preventDefault();
+}
    
 
    render() { //Whenever our class runs, render method will be called automatically, it may have already defined in the constructor behind the scene.
       return (
+          <div>
           
-        <ul class="menu">
+          <ul class="menu">
+          <li name=""><a href="http://localhost:3000/Dashboard">Dashboard</a></li>
  
+          <li name="abc"><a href="http://localhost:3000/Home">Log-Out </a></li>
+          </ul>
+
+
+
+
+<div class="row11">
+<div class="col11">
+
+<h1></h1>
+<div class="select">
+  <select name="slct" id="slct">
+    <option selected disabled>Select Department</option>
+    <option value="Information Technology">Information Technology</option>
+    <option value="Electronics">Electronics</option>
+    <option value="Electronics and Telecommunication">Electronics and Telecommunication</option>
+    <option value="Computer">Computer</option>
+  </select>
+  </div>
  
-    <li><a href="http://localhost:3001/Studentdata">Department</a>
-    <ul>
-            <li><a href="#" class="documents">Information Technology</a></li>
-            <li><a href="#" class="messages">Electronics</a></li>
-            <li><a href="#" class="signout">Electronics and telecommunication</a></li>
-            <li><a href="#" class="signout">Computer</a></li>
-        </ul>
-        </li>
-    <li><a href="#">Class</a>
- 
-        <ul> 
-            <li><a href="#" class="signout">Elite+gold</a></li>
-            <li><a href="#" class="messages">Elite+silver</a></li>
-            <li><a href="#" class="documents">Elite</a></li>
-            <li><a href="#" class="messages">Successfully completed</a></li>
-            <li><a href="#" class="messages">Below 40</a></li>
-        </ul>
- 
-    </li>
-    <li><a href="#">Year</a>
-    <ul> 
-            <li><a href="#" class="signout">2016</a></li>
-            <li><a href="#" class="messages">2017</a></li>
-            <li><a href="#" class="documents">2018</a></li>
-            <li><a href="#" class="messages">2019</a></li>
-            <li><a href="#" class="signout">2020</a></li>
-            <li><a href="#" class="messages">2022</a></li>
-            <li><a href="#" class="documents">2023</a></li>
-            <li><a href="#" class="messages">2024</a></li>
-        </ul>
-        </li>
-        <li name="abc"><a href="http://localhost:3001/Home">Log-Out</a></li>
-        
-            <li name="abc"><a href="#">Excel-Download</a></li>
- 
-    
-        <li name="xyz"><a href="#">Go</a></li>
-        
-</ul> 
+
+</div>
+
+
+<div class="col11">
+
+<h1></h1>
+<div class="select">
+  <select name="slct" id="slct">
+    <option selected disabled>Select Class</option>
+    <option value="1">Elite+Gold</option>
+    <option value="2">Elite+Silver</option>
+    <option value="3">Elite</option>
+    <option value="4">Successfully-Completed</option>
+    <option value="5">Below-40</option>
+  </select>
+  </div>
+</div><div class="col11">
+
+<h1></h1>
+<div class="select">
+  <select name="slct" id="slct">
+    <option selected disabled>Select Year</option>
+    <option value="1">2016</option>
+    <option value="2">2017</option>
+    <option value="3">2018</option>
+    <option value="4">2019</option>
+    <option value="5">2020</option>
+    <option value="6">2021</option>
+    <option value="7">2022</option>
+    <option value="8">2023</option>
+    <option value="9">2024</option>
+  </select>
+  </div>
+</div>
+
+    </div>
+    <div>
+    <button type="button" onClick={this.handleSubmit4}>Try it</button>
+    </div>
+    <p id="demo"></p>
+
+
+
+</div>
       )
    }
 }
